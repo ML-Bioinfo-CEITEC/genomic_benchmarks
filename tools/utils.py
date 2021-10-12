ca4dc44c872457b8ef8763cbbadf1fbe1ecf314f
davidcechak <davidcechak@gmail.com>
2021-10-12 04:49:47 +0000

    Add coll function in utils

--- tools/utils.py
+++ tools/utils.py
@@ -8,14 +8,33 @@ def coll(batch):
         'C':1,
         'T':2,
         'G':3,
+        'N':4,
     }
     xs, ys = [],[]
     for text,label in batch:
         ys.append(torch.tensor([label], dtype=torch.float32))
         # xs.append(torch.tensor([mapper[ch] for ch in text]).to('cuda'))
-        xs.append(torch.tensor([mapper[ch] for ch in text], dtype=torch.float32)) #TODO int doesnt work, why?
+        tmp = [mapper[ch] for ch in text]
         
+#         Todo: padding
+        if(False):
+#             for i in range(4707 - len(tmp)):
+#                 tmp.append(4)
+            pad = nn.ConstantPad2d(0, (0,4707 - len(tmp),0,0))
+            tmp = pad(tmp)
+        x = torch.tensor(tmp, dtype=torch.float32)
+        xs.append(x) #TODO int doesnt work, why?
     
     xs = torch.stack(xs)
     ys = torch.stack(ys)
     return xs.to('cuda'),ys.to('cuda')
+
+
+#             torch.nn.functional.pad(
+#                 torch.tensor([mapper[ch] for ch in text], dtype=torch.float32), 
+#                 (0,4707), 
+#                 mode='constant', 
+#                 value=4
+#             )
+
+
