266d1931b332374938ab7472244e16d6a19991b1
davidcechak <davidcechak@gmail.com>
2021-10-18 12:02:00 +0000

    Extract model

---
+++ notebooks/cnn_model.py
@@ -0,0 +1,128 @@
+import os
+import numpy as np
+import torch
+from torch import nn
+
+# A simple CNN model
+class CNN(nn.Module):
+        
+    def __init__(self, number_of_classes, vocab_size, embedding_dim, input_len):
+        super(CNN, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.conv1 = nn.Conv1d(in_channels=embedding_dim, out_channels=16, kernel_size=8, bias=True)
+        self.norm1 = nn.BatchNorm1d(16)
+        self.relu = nn.ReLU()
+        self.pool1 = nn.MaxPool1d(2)
+        
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=8, kernel_size=8, bias=True)
+        self.norm2 = nn.BatchNorm1d(8)
+        self.pool2 = nn.MaxPool1d(2)
+
+        self.conv3 = nn.Conv1d(in_channels=8, out_channels=4, kernel_size=8, bias=True)
+        self.norm3 = nn.BatchNorm1d(4)
+        self.pool3 = nn.MaxPool1d(2)
+
+#         compute output shape of conv layers   
+        self.flatten = nn.Flatten()
+        self.lin1 = nn.Linear(self.count_flatten_size(input_len), 512)
+        self.lin2 = nn.Linear(512, number_of_classes)
+        self.sigmoid = nn.Sigmoid()
+        self.loss = torch.nn.functional.binary_cross_entropy_with_logits
+
+    def count_flatten_size(self, input_len):        
+        zeros = torch.zeros([1, input_len], dtype=torch.long)
+        x = self.embeddings(zeros)
+        x = x.transpose(1, 2)
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu(x)
+        x = self.pool3(x)
+
+        x = self.flatten(x)
+        return x.size()[1]
+        
+    def forward(self, x):
+        x = self.embeddings(x)
+        x = x.transpose(1, 2)
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
+        
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+        
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu(x)
+        x = self.pool3(x)
+        
+        x = self.flatten(x)
+        x = self.lin1(x) 
+        x = self.lin2(x) 
+        x = self.sigmoid(x)
+        return x
+
+        
+    def train_loop(self, dataloader, optimizer):
+        for x, y in dataloader:
+            optimizer.zero_grad()
+            pred = self(x)
+            loss = self.loss(pred, y)
+            loss.backward()
+            optimizer.step()
+        
+#       train acc
+# todo: optimize counting of acc
+        size = dataloader.dataset.__len__()
+        num_batches = len(dataloader)
+        train_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for X, y in dataloader:
+                pred = self(X)
+                train_loss += self.loss(pred, y).item()
+                correct += (torch.round(pred) == y).sum().item()
+
+        train_loss /= num_batches
+        correct /= size
+        print(f"Train metrics: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {train_loss:>8f} \n")
+            
+            
+    def train(self, dataloader, epochs):
+        optimizer = torch.optim.Adam(self.parameters())
+        for t in range(epochs):
+            print(f"Epoch {t}")
+            self.train_loop(dataloader, optimizer)
+
+    def test(self, dataloader):
+        size = dataloader.dataset.__len__()
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for X, y in dataloader:
+                pred = self(X)
+                test_loss += self.loss(pred, y).item()
+                correct += (torch.round(pred) == y).sum().item()
+
+        print('test_loss ', test_loss)
+        print('num_batches', num_batches)
+        print('correct', correct)
+        print('size', size)
+
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,36 +1,16 @@
 import torch 
 import numpy as np
+from collections import Counter 
 from torch.nn import ConstantPad1d
+from torchtext.vocab import vocab, build_vocab_from_iterator
 
 
 def simple_coll_factory(vocab, tokenizer):
     def simple_coll(batch):
-#         mapper = {
-#             'A':0,
-#             'C':1,
-#             'T':2,
-#             'G':3,
-#             'N':4,
-#         }
-        xs, ys = [],[]
-#         for text,label in batch:
-#             ys.append(torch.tensor([label], dtype=torch.float32))
-#             # xs.append(torch.tensor([mapper[ch] for ch in text]).to('cuda'))
-#             tmp = [mapper[ch] for ch in text]
-#             x = torch.tensor(tmp, dtype=torch.long)
-#             xs.append(x) #TODO int doesnt work, why?
-        
+        xs, ys = [],[]      
         for text,label in batch:
             ys.append(torch.tensor([label], dtype=torch.float32))
             tmp = torch.tensor([vocab[token] for token in tokenizer(text)], dtype=torch.long)
-            
-#             tmp = [mapper[ch] for ch in text]
-    #         maybe not necessary
-#             tmp = torch.tensor(tmp, dtype=torch.float32)
-    #       Todo: padding
-#             pad = ConstantPad1d((0, longest_length - len(tmp)), PAD_IDX)
-#             tmp = pad(tmp)
-            
             x = torch.tensor(tmp, dtype=torch.long)
             xs.append(x)
             
@@ -43,23 +23,10 @@ def simple_coll_factory(vocab, tokenizer):
 def padding_coll_factory(longest_length, vocab, tokenizer):
     def padding_coll(batch):
         PAD_IDX = vocab['<pad>']
-        
-#         mapper = {
-#             'A':0,
-#             'C':1,
-#             'T':2,
-#             'G':3,
-#             'N':4,
-#         }
         xs, ys = [],[]
         for text,label in batch:
             ys.append(torch.tensor([label], dtype=torch.float32))
             tmp = torch.tensor([vocab[token] for token in tokenizer(text)], dtype=torch.long)
-            
-#             tmp = [mapper[ch] for ch in text]
-    #         maybe not necessary
-#             tmp = torch.tensor(tmp, dtype=torch.float32)
-    #       Todo: padding
             pad = ConstantPad1d((0, longest_length - len(tmp)), PAD_IDX)
             tmp = pad(tmp)
             
@@ -68,8 +35,6 @@ def padding_coll_factory(longest_length, vocab, tokenizer):
 
         xs = torch.stack(xs)
         ys = torch.stack(ys)
-#         print(xs.size())
-#         print(ys.size())
         return xs.to('cuda'),ys.to('cuda')
     
     return padding_coll
@@ -82,3 +47,33 @@ def padding_coll_factory(longest_length, vocab, tokenizer):
 #             )
 
 
+class LetterTokenizer():
+    def __init__(self, **kwargs):
+        pass
+    def __call__(self, items):
+        if isinstance(items, str):
+            return self.__tokenize_str(items)
+        else:
+            return (self.__tokenize_str(t) for t in items)
+    def __tokenize_str(self, t):
+        tokenized = list(t.replace("\n",""))
+        tokenized.append('<eos>')
+        tokenized.insert(0,'<bos>')
+        return tokenized
+    
+
+def build_vocab(dataset, tokenizer, use_padding):
+    counter = Counter()
+    for i in range(len(dataset)):
+        counter.update(tokenizer(dataset[i][0]))
+#     print(counter.most_common())
+    builded_voc = vocab(counter)
+    if(use_padding):
+        builded_voc.append_token('<pad>')
+    return builded_voc
+
+# todo: why build fn does not work as expected (iterator argument)
+#     return build_vocab_from_iterator(
+#         iterator = counter, 
+#         specials = ['<unk>', '<pad>', '<bos>', '<eos>'],
+#         special_first = True)
