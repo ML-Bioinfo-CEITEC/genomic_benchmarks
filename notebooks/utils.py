1502ba350ca998b1ca24a448055fd2c981b7ffab
davidcechak <davidcechak@gmail.com>
2021-10-18 11:20:38 +0000

    Add coding vs intergen demo

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,27 +2,42 @@ import torch
 import numpy as np
 from torch.nn import ConstantPad1d
 
-# TODO add one-hot encoding
 
-def simple_coll(batch):
-    mapper = {
-        'A':0,
-        'C':1,
-        'T':2,
-        'G':3,
-        'N':4,
-    }
-    xs, ys = [],[]
-    for text,label in batch:
-        ys.append(torch.tensor([label], dtype=torch.float32))
-        # xs.append(torch.tensor([mapper[ch] for ch in text]).to('cuda'))
-        tmp = [mapper[ch] for ch in text]
-        x = torch.tensor(tmp, dtype=torch.float32)
-        xs.append(x) #TODO int doesnt work, why?
-    
-    xs = torch.stack(xs)
-    ys = torch.stack(ys)
-    return xs.to('cuda'),ys.to('cuda')
+def simple_coll_factory(vocab, tokenizer):
+    def simple_coll(batch):
+#         mapper = {
+#             'A':0,
+#             'C':1,
+#             'T':2,
+#             'G':3,
+#             'N':4,
+#         }
+        xs, ys = [],[]
+#         for text,label in batch:
+#             ys.append(torch.tensor([label], dtype=torch.float32))
+#             # xs.append(torch.tensor([mapper[ch] for ch in text]).to('cuda'))
+#             tmp = [mapper[ch] for ch in text]
+#             x = torch.tensor(tmp, dtype=torch.long)
+#             xs.append(x) #TODO int doesnt work, why?
+        
+        for text,label in batch:
+            ys.append(torch.tensor([label], dtype=torch.float32))
+            tmp = torch.tensor([vocab[token] for token in tokenizer(text)], dtype=torch.long)
+            
+#             tmp = [mapper[ch] for ch in text]
+    #         maybe not necessary
+#             tmp = torch.tensor(tmp, dtype=torch.float32)
+    #       Todo: padding
+#             pad = ConstantPad1d((0, longest_length - len(tmp)), PAD_IDX)
+#             tmp = pad(tmp)
+            
+            x = torch.tensor(tmp, dtype=torch.long)
+            xs.append(x)
+            
+        xs = torch.stack(xs)
+        ys = torch.stack(ys)
+        return xs.to('cuda'),ys.to('cuda')
+    return simple_coll
 
 
 def padding_coll_factory(longest_length, vocab, tokenizer):
