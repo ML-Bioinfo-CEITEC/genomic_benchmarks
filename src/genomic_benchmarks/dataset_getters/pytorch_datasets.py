6e54e1fdd5c1ca332acc935ed2a1c8a9913030dd
Vlasta <martinekvlastimil95@gmail.com>
2021-10-18 11:42:00 +0000

    Add version parameter to dsets

--- src/genomic_benchmarks/dataset_getters/pytorch_datasets.py
+++ src/genomic_benchmarks/dataset_getters/pytorch_datasets.py
@@ -9,7 +9,7 @@ class GenomicClfDataset(Dataset):
     A class to represent generic genomic classification pytorch dataset.
     Instance of this class can be directly wrapped by pytorch DataLoader
     '''
-    def __init__(self, dset_name, split, force_download=False):
+    def __init__(self, dset_name, split, force_download=False, version=None):
         '''
         Parameters
             dset_name : str
@@ -18,6 +18,8 @@ class GenomicClfDataset(Dataset):
                 "train" or "test"
             force_download : bool
                 Whether to re-download already existing files
+            version : int
+                Version of the dataset
         '''
         translated_datasets_folder_path = Path.home() / '.genomic_benchmarks'
         # TODO resolve path some other way?
@@ -28,7 +30,7 @@ class GenomicClfDataset(Dataset):
 
         destination_path = Path(
             f'{translated_datasets_folder_path}/translated_{dset_name}')
-        base_path = download_dataset(dset_path, version=None, dest_path=destination_path,
+        base_path = download_dataset(dset_path, version=version, dest_path=destination_path,
                                      cache_path=fasta_cache_path, force_download=force_download)
 
         if(split == 'train' or split == 'test'):
@@ -60,14 +62,14 @@ class GenomicClfDataset(Dataset):
         return x, y
 
 
-def get_dataset(dataset_name, split, force_download=False):
-    return GenomicClfDataset(dataset_name, split, force_download)
+def get_dataset(dataset_name, split, force_download=False, version=None):
+    return GenomicClfDataset(dataset_name, split, force_download, version)
 
 
-def DemoCodingVsIntergenomicSeqs(split, force_download=False):
-    return GenomicClfDataset('demo_coding_vs_intergenomic_seqs', split, force_download)
+def DemoCodingVsIntergenomicSeqs(split, force_download=False, version=None):
+    return GenomicClfDataset('demo_coding_vs_intergenomic_seqs', split, force_download, version)
 
 
-def DemoMouseEnhancers(split, force_download=False):
-    return GenomicClfDataset('demo_mouse_enhancers', split, force_download)
+def DemoMouseEnhancers(split, force_download=False, version=None):
+    return GenomicClfDataset('demo_mouse_enhancers', split, force_download, version)
 
