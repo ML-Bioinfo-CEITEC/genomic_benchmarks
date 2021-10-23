e0e51ccbf2436bc3428d60ea96a60de753180e90
Katarína Grešová <514001@mail.muni.cz>
2021-10-23 12:47:02 +0200

    class name to match dataset name

--- src/genomic_benchmarks/dataset_getters/pytorch_datasets.py
+++ src/genomic_benchmarks/dataset_getters/pytorch_datasets.py
@@ -74,5 +74,5 @@ def DemoMouseEnhancers(split, force_download=False, version=None):
     return GenomicClfDataset('demo_mouse_enhancers', split, force_download, version)
 
 
-def DemoHumanNontataPromoters(split, force_download=False, version=None):
+def HumanNontataPromoters(split, force_download=False, version=None):
     return GenomicClfDataset('human_nontata_promoters', split, force_download, version)
