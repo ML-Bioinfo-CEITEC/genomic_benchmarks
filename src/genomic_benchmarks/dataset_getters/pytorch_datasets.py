2eec0d5a6bbc937596256aebd36c8b24ae978e41
Vlasta <martinekvlastimil95@gmail.com>
2021-11-05 07:31:40 +0000

    Fix re-downloading in pytorch, reuse paths

--- src/genomic_benchmarks/dataset_getters/pytorch_datasets.py
+++ src/genomic_benchmarks/dataset_getters/pytorch_datasets.py
@@ -1,42 +1,34 @@
+from genomic_benchmarks.data_check import is_downloaded
+from genomic_benchmarks.loc2seq.with_biopython import CACHE_PATH, download_dataset
 from torch.utils.data import Dataset
-from pathlib import Path
-import genomic_benchmarks
-from genomic_benchmarks.loc2seq.with_biopython import download_dataset
-import os
+
 
 class GenomicClfDataset(Dataset):
-    '''
+    """
     A class to represent generic genomic classification pytorch dataset.
     Instance of this class can be directly wrapped by pytorch DataLoader
-    '''
+    """
+
     def __init__(self, dset_name, split, force_download=False, version=None):
-        '''
+        """
         Parameters
             dset_name : str
                 One of the existing dataset names, list available at TODO
             split : str
-                "train" or "test"
+                train' or 'test'
             force_download : bool
                 Whether to re-download already existing files
             version : int
                 Version of the dataset
-        '''
-        translated_datasets_folder_path = Path.home() / '.genomic_benchmarks'
-        # TODO resolve path some other way?
-        interval_datasets_folder_path = Path(os.path.dirname(
-            genomic_benchmarks.__file__)).parent.parent/'datasets'
-        fasta_cache_path = translated_datasets_folder_path / 'fasta'
-        dset_path = Path(f'{interval_datasets_folder_path}/{dset_name}')
+        """
+        base_path = CACHE_PATH / dset_name
+        if not is_downloaded(dset_name):
+            download_dataset(dset_name, version=version, force_download=force_download)
 
-        destination_path = Path(
-            f'{translated_datasets_folder_path}/translated_{dset_name}')
-        base_path = download_dataset(dset_path, version=version, dest_path=destination_path,
-                                     cache_path=fasta_cache_path, force_download=force_download)
-
-        if(split == 'train' or split == 'test'):
-            base_path = base_path/split
+        if split == "train" or split == "test":
+            base_path = base_path / split
         else:
-            raise Exception('Define split, train or test')
+            raise Exception("Define split, train or test")
 
         self.all_paths = []
         self.all_labels = []
@@ -55,7 +47,7 @@ class GenomicClfDataset(Dataset):
 
     def __getitem__(self, idx):
         txt_path = self.all_paths[idx]
-        with open(txt_path, 'r') as f:
+        with open(txt_path, "r") as f:
             content = f.read()
         x = content
         y = self.all_labels[idx]
@@ -67,24 +59,24 @@ def get_dataset(dataset_name, split, force_download=False, version=None):
 
 
 def DemoCodingVsIntergenomicSeqs(split, force_download=False, version=None):
-    return GenomicClfDataset('demo_coding_vs_intergenomic_seqs', split, force_download, version)
+    return GenomicClfDataset("demo_coding_vs_intergenomic_seqs", split, force_download, version)
 
 
 def DemoHumanOrWorm(split, force_download=False, version=None):
-    return GenomicClfDataset('demo_human_or_worm', split, force_download, version)
+    return GenomicClfDataset("demo_human_or_worm", split, force_download, version)
 
 
 def DemoMouseEnhancers(split, force_download=False, version=None):
-    return GenomicClfDataset('demo_mouse_enhancers', split, force_download, version)
+    return GenomicClfDataset("demo_mouse_enhancers", split, force_download, version)
 
 
 def HumanEnhancersCohn(split, force_download=False, version=None):
-    return GenomicClfDataset('human_enhancers_cohn', split, force_download, version)
+    return GenomicClfDataset("human_enhancers_cohn", split, force_download, version)
 
 
 def HumanEnhancersEnsembl(split, force_download=False, version=None):
-    return GenomicClfDataset('human_enhancers_ensembl', split, force_download, version)
+    return GenomicClfDataset("human_enhancers_ensembl", split, force_download, version)
 
 
 def HumanNontataPromoters(split, force_download=False, version=None):
-    return GenomicClfDataset('human_nontata_promoters', split, force_download, version)
+    return GenomicClfDataset("human_nontata_promoters", split, force_download, version)
