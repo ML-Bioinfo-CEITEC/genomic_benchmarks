2c96ad9494d624e3442d9d77aaef1c915170dcd0
Petr Simecek <lamparna@gmail.com>
2021-11-23 08:50:33 +0100

    fix: run all echo

--- experiments/utils/run_all_tf_experiments.sh
+++ experiments/utils/run_all_tf_experiments.sh
@@ -3,6 +3,6 @@ DATASETS=`ls -1d ../datasets/*/`
 for d in $DATASETS 
 do
   dt=`basename $d`
-  echo "Processing dataset papermill utils/tf_cnn_classifier.ipynb tf_cnn_experiments/[DATASET NAME].ipynb -p DATASET [DATASET NAME]\n\n"
+  echo "Processing dataset $dt\n\n"
   papermill utils/tf_cnn_classifier.ipynb tf_cnn_experiments/$dt.ipynb -p DATASET $dt
 done
