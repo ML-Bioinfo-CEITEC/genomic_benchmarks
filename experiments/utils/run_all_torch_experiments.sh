6c949d49f7e1eaa2b91d29cc70f89600ed659dc3
davidcechak <davidcechak@gmail.com>
2021-11-25 15:58:11 +0000

    Add F1 score to torch experiments

---
+++ experiments/utils/run_all_torch_experiments.sh
@@ -0,0 +1,8 @@
+DATASETS=`ls -1d ../datasets/*/`
+
+for d in $DATASETS 
+do
+  dt=`basename $d`
+  echo "Processing dataset $dt\n\n"
+  papermill utils/torch_cnn_classifier.ipynb torch_cnn_experiments/$dt.ipynb -p DATASET $dt -p EPOCHS 1
+done
