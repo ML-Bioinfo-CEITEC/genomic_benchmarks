ebfcb5601f2923384608d91d6b5ca65499bb22e4
davidcechak <davidcechak@gmail.com>
2022-01-27 08:52:23 +0000

    Add fallback and comet

--- experiments/utils/run_all_torch_experiments.sh
+++ experiments/utils/run_all_torch_experiments.sh
@@ -1,8 +1,28 @@
+#!/bin/sh
 DATASETS=`ls -1d ../datasets/*/`
 
-for d in $DATASETS 
-do
-  dt=`basename $d`
-  echo "Processing dataset $dt\n\n"
-  papermill utils/torch_cnn_classifier.ipynb torch_cnn_experiments/$dt.ipynb -p DATASET $dt -p EPOCHS 10
+
+for i in 1 2 3 4 5; do
+  echo $i
+  for d in $DATASETS 
+  do
+    dt=`basename $d`
+    echo "Processing dataset $dt\n\n"
+    papermill utils/torch_cnn_classifier.ipynb torch_cnn_experiments/$dt.ipynb -p DATASET $dt -p EPOCHS 200 -p ITER $i -p PATIENCE 30
+  done
 done
+
+
+# END=5
+# for ((i=1;i<=END;i++)); do
+#   echo $i
+# done
+
+# for d in $DATASETS 
+# do
+#   dt=`basename $d`
+#   echo "Processing dataset $dt\n\n"
+#   papermill utils/torch_cnn_classifier.ipynb torch_cnn_experiments/$dt.ipynb -p DATASET $dt -p EPOCHS 10
+# done
+
+
--- src/genomic_benchmarks/models/torch.py
+++ src/genomic_benchmarks/models/torch.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from pathlib import Path
 
 import numpy as np
 
@@ -88,7 +89,7 @@ class CNN(nn.Module):
         x = self.output_activation(x)
         return x
 
-    def train_loop(self, dataloader, valid_dataloader, optimizer):
+    def train_loop(self, dataloader, valid_dataloader, optimizer, experiment):
         for x, y in dataloader:
             optimizer.zero_grad()
             pred = self(x)
@@ -123,34 +124,46 @@ class CNN(nn.Module):
         valid_loss /= valid_num_batches
         valid_correct /= valid_size
 
+        experiment.log_metric("train_loss", train_loss)
+        experiment.log_metric("valid_loss", valid_loss)
+        experiment.log_metric("valid_acc", valid_correct)
+
         print(f"Valid metrics: \n Accuracy: {(100*valid_correct):>0.1f}%, Avg loss: {valid_loss:>8f} \n")
         print(f"Train metrics: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {train_loss:>8f} \n")
         return valid_loss
 
-    def train(self, dataloader, valid_loader, epochs):
+    def train(self, dataloader, valid_loader, epochs, patience, checkpoint_name, experiment):
         optimizer = torch.optim.Adam(self.parameters())
         best_valid_so_far = sys.float_info.max
         epochs_not_improving = 0
-        patience = 5
+        # TODO set: patience, model_path
+        # patience = 5
         best_epoch = 0
+        checkpoint_name = checkpoint_name + "_checkpoint.pt"
+        model_path = Path.home() / "genomic_benchmarks" / "checkpoints" / checkpoint_name
         print(best_valid_so_far)
         for t in range(epochs):
             print(f"Epoch {t}")
-            valid_loss = self.train_loop(dataloader, valid_loader, optimizer)
+            valid_loss = self.train_loop(dataloader, valid_loader, optimizer, experiment)
             if valid_loss <= best_valid_so_far:
-                print("new best valid loss")
                 best_valid_so_far = valid_loss
                 epochs_not_improving = 0
                 best_epoch = t
+                print("new best valid loss in epoch " + str(best_epoch) + " -> saving new checkpoint")
+                # save model
+                torch.save(self.state_dict(), model_path)
             else:
                 epochs_not_improving += 1
                 print("not improved for epochs:", epochs_not_improving)
-                if epochs_not_improving == patience:
-                    pass
-                    # TODO fallback to best model
+                if epochs_not_improving >= patience:
+                    # load model
+                    self.load_state_dict(torch.load(model_path))
+                    print("ending training")
+                    print("loading best model from epoch " + str(best_epoch))
+                    break
 
     # TODO: update for multiclass classification datasets
-    def test(self, dataloader, positive_label=1):
+    def test(self, dataloader, positive_label=1, experiment=None):
         size = dataloader.dataset.__len__()
         num_batches = len(dataloader)
         test_loss, correct = 0, 0
@@ -182,5 +195,11 @@ class CNN(nn.Module):
         test_loss /= num_batches
         accuracy = correct / size
         print(f"Test metrics: \n Accuracy: {accuracy:>6f}, F1 score: {f1_score:>6f}, Avg loss: {test_loss:>6f} \n")
+
+        experiment.log_metric("test_F1_score", f1_score)
+        experiment.log_metric("test_acc", accuracy)
+        experiment.log_metric("test_avg_loss", test_loss)
+        experiment.log_metric("recall", recall)
+        experiment.log_metric("precision", precision)
 
         return accuracy, f1_score
