3478376e4c7b18d1d876bed4ea817f39d85f1668
Katarína Grešová <514001@mail.muni.cz>
2022-06-02 23:46:41 +0200

    tf multiclass

--- src/genomic_benchmarks/models/tf.py
+++ src/genomic_benchmarks/models/tf.py
@@ -16,23 +16,43 @@ vectorize_layer = TextVectorization(output_mode="int", split=character_split_fn)
 # one-hot encoding
 onehot_layer = tf.keras.layers.Lambda(lambda x: tf.one_hot(tf.cast(x, "int64"), 4))
 
-# Binary F1 score
-binary_f1_score = tfa.metrics.F1Score(num_classes=1, threshold=0.5, average="micro")
+def get_basic_cnn_model_v0(num_classes):
 
-basic_cnn_model_v0 = tf.keras.Sequential(
-    [
-        onehot_layer,
-        Conv1D(32, kernel_size=8, data_format="channels_last", activation="relu"),
-        BatchNormalization(),
-        MaxPooling1D(),
-        Conv1D(16, kernel_size=8, data_format="channels_last", activation="relu"),
-        BatchNormalization(),
-        MaxPooling1D(),
-        Conv1D(4, kernel_size=8, data_format="channels_last", activation="relu"),
-        BatchNormalization(),
-        MaxPooling1D(),
-        Dropout(0.3),
-        GlobalAveragePooling1D(),
-        Dense(1),
-    ]
-)
+    if num_classes == 2:
+        num_classes = 1
+
+    model = tf.keras.Sequential(
+        [
+            onehot_layer,
+            Conv1D(32, kernel_size=8, data_format="channels_last", activation="relu"),
+            BatchNormalization(),
+            MaxPooling1D(),
+            Conv1D(16, kernel_size=8, data_format="channels_last", activation="relu"),
+            BatchNormalization(),
+            MaxPooling1D(),
+            Conv1D(4, kernel_size=8, data_format="channels_last", activation="relu"),
+            BatchNormalization(),
+            MaxPooling1D(),
+            Dropout(0.3),
+            GlobalAveragePooling1D(),
+            Dense(num_classes), # we don't need softmax, because entropy uses argmax and argmax of logits and probabilities are same.
+        ]
+    )
+
+    if num_classes == 1:
+        loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        acc = tf.metrics.BinaryAccuracy()
+        f1 = tfa.metrics.F1Score(num_classes=1, threshold=0.5, average="micro")
+    else:
+        loss = 'categorical_crossentropy'
+        acc = tf.metrics.CategoricalAccuracy()
+        f1 = tfa.metrics.F1Score(num_classes=num_classes, average="micro")
+
+    model.compile(
+        loss=loss,
+        optimizer='adam',
+        metrics=[acc, f1]
+    )
+
+    return model
+
