d4932d856888c3d78f4ff14a146a541afb2e6721
Katarína Grešová <514001@mail.muni.cz>
2022-06-06 10:54:39 +0200

    up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="genomic_benchmarks",
-    version="0.0.6",
+    version="0.0.7",
     description="Genomic Benchmarks",
     long_description=long_description,
     long_description_content_type="text/markdown",
