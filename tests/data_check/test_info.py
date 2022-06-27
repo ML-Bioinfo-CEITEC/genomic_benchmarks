e8ba8fb5a9ca4092d351ee9fad73efabbd4eff12
Petr Simecek <lamparna@gmail.com>
2022-06-27 22:48:03 +0200

    test_info corner case added

--- tests/data_check/test_info.py
+++ tests/data_check/test_info.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 from genomic_benchmarks.data_check import info
 
 
@@ -9,3 +10,8 @@ def test_info():
     assert isinstance(mouse_info, pd.DataFrame)
     assert mouse_info.shape == (2, 2)
     assert mouse_info.train.sum() == 968
+
+
+def test_info_fails_for_missing_dataset():
+    with pytest.raises(FileNotFoundError):
+        info("donotexist")
