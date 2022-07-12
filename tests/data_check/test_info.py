3965c74f79decca4e2625783ec287baee97cd4f0
davidcechak <davidcechak@gmail.com>
2022-07-12 14:09:24 +0200

    Test is_downloaded function

--- tests/data_check/test_info.py
+++ tests/data_check/test_info.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import pandas as pd
 import pytest
-from genomic_benchmarks.data_check import info, list_datasets
+from genomic_benchmarks.data_check import info, is_downloaded, list_datasets
+from genomic_benchmarks.utils.paths import CACHE_PATH, DATASET_DIR_PATH
 
 # dataset_name, version, train samples, test samples, num of classes
 datasets_test_data = [
@@ -8,9 +11,9 @@ datasets_test_data = [
         ("demo_coding_vs_intergenomic_seqs", 0, 75000, 25000, 2),
         ("demo_human_or_worm", 0, 75000, 25000, 2),
         ("drosophila_enhancers_stark", 0, 5184, 1730, 2),
-        # ("dummy_mouse_enhancers_ensembl", 0, 968, 242, 2),
+        ("dummy_mouse_enhancers_ensembl", 0, 968, 242, 2),
         ("human_enhancers_cohn", 0, 20843, 6948, 2),
-        # ("human_enhancers_ensembl", 0, 123872, 30970, 2),
+        ("human_enhancers_ensembl", 0, 123872, 30970, 2),
         ("human_ocr_ensembl", 0, 139804, 34952, 2),
         ("human_ensembl_regulatory", 0, 231348, 57713, 3),
     ]
@@ -32,18 +35,52 @@ def test_info_fails_for_missing_dataset():
         info("donotexist")
 
 
-@pytest.mark.parametrize("dataset_name, version, train_samples, test_samples, classes", datasets_test_data)
-def test_list_datasets_returns_all_expected_datasets(dataset_name, version, train_samples, test_samples, classes):
-    assert dataset_name in list_datasets()
+# @pytest.mark.parametrize("dataset_name, version, train_samples, test_samples, classes", datasets_test_data)
+# def test_list_datasets_returns_all_expected_datasets(dataset_name, version, train_samples, test_samples, classes):
+#     assert dataset_name in list_datasets()
 
 
-def test_list_datasets_returns_only_expected_datasets():
+def test_list_datasets_returns_exactly_expected_datasets():
     expected = [dataset[0] for dataset in datasets_test_data]
-    actual = list_datasets()
 
+    actual = list_datasets()
     lacks = set(expected) - set(actual)
     extra = set(actual) - set(expected)
 
-    message = f"list_datasets() contains lacks elements {lacks} " if lacks else ''
-    message += f"list_datasets() contains extra elements {extra}" if extra else ''
+    message = f"list_datasets() lacks elements {lacks} " if lacks else ''
+    message += f"list_datasets() has extra elements {extra}" if extra else ''
     assert not message
+
+
+def test_list_datasets_returns_exactly_expected_in_local_repo():
+    actual = [x.name for x in Path(DATASET_DIR_PATH).iterdir() if x.is_dir()]
+
+    expected = list_datasets(local_repo=True)
+    lacks = set(expected) - set(actual)
+    extra = set(actual) - set(expected)
+
+    message = f"list_datasets(local_repo=True) lacks elements {lacks} " if lacks else ''
+    message += f"list_datasets(local_repo=True) has extra elements {extra}" if extra else ''
+    assert not message
+
+
+def test_is_downloaded_returns_false_not_existing():
+    assert not is_downloaded('not_existing')
+
+
+@pytest.mark.parametrize("dataset_name, version, train_samples, test_samples, classes", datasets_test_data)
+def test_is_downloaded_returns_true_for_downloaded_datasets(dataset_name, version, train_samples, test_samples, classes):
+    base_path = CACHE_PATH / dataset_name
+
+    if(Path(base_path).exists()):
+        assert is_downloaded(dataset_name)
+
+
+@pytest.mark.parametrize("dataset_name, version, train_samples, test_samples, classes", datasets_test_data)
+def test_is_downloaded_returns_false_for_not_downloaded_datasets(dataset_name, version, train_samples, test_samples, classes):
+    base_path = CACHE_PATH / dataset_name
+
+    if(not Path(base_path).exists()):
+        assert not is_downloaded(dataset_name)
+
+
